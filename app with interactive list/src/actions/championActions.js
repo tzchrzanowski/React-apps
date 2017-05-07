a1c5ffcb2aa66f3e1405bea4a0f0c13b417dcfcf
Tom <tzchrzanowski@gmail.com>
2017-05-07 16:35:14 +0200

    Add files via upload
    Simple app with interactive list of items.

---
+++ app with interactive list/src/actions/championActions.js
@@ -0,0 +1,3 @@
+export function createChampion(champion){
+    return {type: 'CREATE_CHAMPION', champion};
+}
---
+++ app with interactive list/src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div className="jumbotron">
+                <h2>What's it all about?</h2>
+                <p>This site is a simple guide focused on current meta in LoL.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
